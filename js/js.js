92d2e2f451c4159276caf3f85bef31e41b0a7b38
agusvlopez <agusvlopez@gmail.com>
2022-12-11 21:52:02 -0300

    agregado de un producto

--- js/js.js
+++ js/js.js
@@ -1,14 +1,15 @@
 const link = document.querySelector('.link');
-link.addEventListener("click",activarModal );
+link.addEventListener("click",activarModal);
 const link2 = document.querySelector('.link2');
-link2.addEventListener("click",activarModal );
+link2.addEventListener("click",activarModal);
 const link3 = document.querySelector('.link3');
-link3.addEventListener("click",activarModal );
+link3.addEventListener("click",activarModal);
 const link4 = document.querySelector('.link4');
 link4.addEventListener("click",activarModal );
 const link5 = document.querySelector('.link5');
-link5.addEventListener("click",activarModal );
-
+link5.addEventListener("click",activarModal);
+const link6 = document.querySelector('.link6');
+link6.addEventListener("click",activarModal);
 
 const cerrar = document.querySelector('#modal-detalles button');
 cerrar.addEventListener("click", cerrarModal);
